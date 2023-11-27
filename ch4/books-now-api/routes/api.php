5ad3cc457019011e45bf3d82f0b621bdba68505a
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2023-11-28 04:08:17 +0600

    Registered all the routes

--- ch4/books-now-api/routes/api.php
+++ ch4/books-now-api/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BookStatusController;
+use App\Http\Controllers\GenreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::apiResource('books', BookController::class);
+
+Route::put('books/status', BookStatusController::class);
+
+Route::apiResource('authors', AuthorController::class);
+Route::apiResource('genres', GenreController::class);
